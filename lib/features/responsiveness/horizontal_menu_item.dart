6347d76a06e9365a6738e0cbc2344441847773d2
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-09 21:39:54 +0000

    add obx to horizontal menu to observe state changes

--- lib/features/responsiveness/horizontal_menu_item.dart
+++ lib/features/responsiveness/horizontal_menu_item.dart
@@ -1,5 +1,6 @@
 import 'package:betticos/core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
 import 'constants/web_controller.dart';
 import 'custom_text.dart';
@@ -18,43 +19,45 @@ class HorizontalMenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 8),
-      child: InkWell(
-        onTap: onTap,
-        onHover: (bool value) {
-          value
-              ? menuController.onHover(route)
-              : menuController.onHover('not hovering');
-        },
-        borderRadius: BorderRadius.circular(30),
-        child: Container(
-          decoration: BoxDecoration(
-            color: menuController.isHovering(route) ||
-                    menuController.isActive(route)
-                ? context.colors.lightGrey
-                : Colors.transparent,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                child: menuController.returnIconFor(route),
-              ),
-              Flexible(
-                child: CustomText(
-                  text: name,
-                  color: context.colors.textDark,
-                  size: !menuController.isActive(route) ? 15 : 16,
-                  weight: !menuController.isActive(route)
-                      ? FontWeight.w600
-                      : FontWeight.bold,
+    return Obx(
+      () => Container(
+        margin: const EdgeInsets.only(bottom: 8),
+        child: InkWell(
+          onTap: onTap,
+          onHover: (bool value) {
+            value
+                ? menuController.onHover(route)
+                : menuController.onHover('not hovering');
+          },
+          borderRadius: BorderRadius.circular(30),
+          child: Container(
+            decoration: BoxDecoration(
+              color: menuController.isHovering(route) ||
+                      menuController.isActive(route)
+                  ? context.colors.lightGrey
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  child: menuController.returnIconFor(route),
                 ),
-              )
-            ],
+                Flexible(
+                  child: CustomText(
+                    text: name,
+                    color: context.colors.textDark,
+                    size: !menuController.isActive(route) ? 15 : 16,
+                    weight: !menuController.isActive(route)
+                        ? FontWeight.w600
+                        : FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
