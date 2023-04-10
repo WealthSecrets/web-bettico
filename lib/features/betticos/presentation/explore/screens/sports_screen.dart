ccef70a0fd4e5bf3da582dd0b7174f14a57859aa
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-10 15:19:10 +0000

    add sports screen, sport bindings and controller

---
+++ lib/features/betticos/presentation/explore/screens/sports_screen.dart
@@ -0,0 +1,46 @@
+import 'package:betticos/core/core.dart';
+import 'package:flutter/material.dart';
+
+import '../../profile/widgets/circle_indicator.dart';
+
+class SportsScreen extends StatelessWidget {
+  const SportsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorColor: context.colors.primary,
+            labelColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+            unselectedLabelStyle: const TextStyle(fontSize: 14),
+            padding: AppPaddings.lH,
+            unselectedLabelColor: Colors.grey,
+            indicator: CircleTabIndicator(
+              color: context.colors.primary,
+              radius: 3,
+            ),
+            tabs: const <Tab>[
+              Tab(text: 'Live Matches'),
+              Tab(text: 'Fixtures'),
+              Tab(text: 'News'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Icon(Icons.flight, size: 350),
+            Icon(Icons.directions_transit, size: 350),
+            Icon(Icons.directions_car, size: 350),
+          ],
+        ),
+      ),
+    );
+  }
+}
