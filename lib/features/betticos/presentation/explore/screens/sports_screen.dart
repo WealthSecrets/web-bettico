f83d53fd8e8066ab1522162f4f05a48d303f6c8a
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-10 15:31:19 +0000

    remove appbar from tabbar

--- lib/features/betticos/presentation/explore/screens/sports_screen.dart
+++ lib/features/betticos/presentation/explore/screens/sports_screen.dart
@@ -12,24 +12,30 @@ class SportsScreen extends StatelessWidget {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          bottom: TabBar(
-            indicatorColor: context.colors.primary,
-            labelColor: Colors.black,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 14,
-            ),
-            unselectedLabelStyle: const TextStyle(fontSize: 14),
-            padding: AppPaddings.lH,
-            unselectedLabelColor: Colors.grey,
-            indicator: CircleTabIndicator(
-              color: context.colors.primary,
-              radius: 3,
-            ),
-            tabs: const <Tab>[
-              Tab(text: 'Live Matches'),
-              Tab(text: 'Fixtures'),
-              Tab(text: 'News'),
+          automaticallyImplyLeading: false,
+          flexibleSpace: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TabBar(
+                indicatorColor: context.colors.primary,
+                labelColor: Colors.black,
+                labelStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+                unselectedLabelStyle: const TextStyle(fontSize: 14),
+                padding: AppPaddings.lH,
+                unselectedLabelColor: Colors.grey,
+                indicator: CircleTabIndicator(
+                  color: context.colors.primary,
+                  radius: 3,
+                ),
+                tabs: const <Tab>[
+                  Tab(text: 'Live Matches'),
+                  Tab(text: 'Fixtures'),
+                  Tab(text: 'News'),
+                ],
+              )
             ],
           ),
         ),
