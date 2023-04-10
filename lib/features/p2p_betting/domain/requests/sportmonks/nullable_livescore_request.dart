ba32e5757f630f155473c175c1117683ea64b8ba
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-10 15:03:56 +0000

    add nullable livescore request model

---
+++ lib/features/p2p_betting/domain/requests/sportmonks/nullable_livescore_request.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: invalid_annotation_target
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'nullable_livescore_request.freezed.dart';
+part 'nullable_livescore_request.g.dart';
+
+@freezed
+class NullLiveScoreRequest with _$NullLiveScoreRequest {
+  const factory NullLiveScoreRequest({
+    int? leagueId,
+  }) = _SNullLiveScoreRequest;
+  factory NullLiveScoreRequest.fromJson(Map<String, dynamic> json) =>
+      _$NullLiveScoreRequestFromJson(json);
+}
