eda6a54fbfd82905bb1d184d058f4ac169455b47
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-12 17:46:20 +0000

    improve market bottom sheet

--- lib/features/betticos/presentation/explore/widgets/market_bottom_sheet.dart
+++ lib/features/betticos/presentation/explore/widgets/market_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:betticos/core/core.dart';
+import 'package:betticos/core/presentation/helpers/responsiveness.dart';
 import 'package:betticos/features/betticos/data/models/listing/listing_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -20,13 +21,17 @@ class MarketBottomSheet extends StatelessWidget {
     final double percentChange1h = listing.quote.usd.percentChange1h;
     final Color color =
         percentChange1h < 0 ? context.colors.error : context.colors.success;
+    final double space = getSpaceValue(context);
     return Stack(
       children: <Widget>[
         Container(
           padding: AppPaddings.mH.add(AppPaddings.bodyT),
           width: 1.sw,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          height: 350,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               const SizedBox(width: 10),
               ClipRRect(
@@ -37,33 +42,119 @@ class MarketBottomSheet extends StatelessWidget {
                   width: 40,
                 ),
               ),
-              const SizedBox(width: 10),
+              const SizedBox(height: 10),
+              Text(
+                listing.name,
+                style: TextStyle(
+                  color: context.colors.textDark,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 20),
               Expanded(
-                child: AppAnimatedColumn(
-                  duration: const Duration(milliseconds: 700),
-                  delay: const Duration(milliseconds: 200),
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      listing.name,
-                      style: TextStyle(
-                        color: context.colors.textDark,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: space),
+                  child: AppAnimatedColumn(
+                    duration: const Duration(milliseconds: 700),
+                    delay: const Duration(milliseconds: 200),
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'PRICE',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      color: context.colors.text,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$${listing.quote.usd.price.toStringAsFixed(2)}',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      color: context.colors.text,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                                ]),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  'CHANGE',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                Text(
+                                  '${percentChange1h.toStringAsFixed(2)}%',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: color,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    const AppSpacing(v: 20),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
+                      const AppSpacing(v: 30),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  'PRICE',
+                                  'PRICE CHART',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 10,
+                                  ),
+                                ),
+                                LineChart(
+                                  width: 50,
+                                  height: 20,
+                                  data: List<LineChartModel>.generate(
+                                    6,
+                                    (_) => LineChartModel(
+                                        amount: Random().nextDouble() * 10),
+                                  ),
+
+                                  linePaint: Paint()
+                                    ..strokeWidth = 2.5
+                                    ..style = PaintingStyle.stroke
+                                    ..color =
+                                        color, // Custom paint for the line
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'VOLUME',
                                   style: TextStyle(
                                     fontWeight: FontWeight.w700,
                                     color: context.colors.text,
@@ -71,158 +162,78 @@ class MarketBottomSheet extends StatelessWidget {
                                   ),
                                 ),
                                 Text(
-                                  '\$${listing.quote.usd.price.toStringAsFixed(2)}',
+                                  listing.quote.usd.volume24h
+                                      .toStringAsFixed(2),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const AppSpacing(v: 30),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'MARKET CAP',
                                   style: TextStyle(
                                     fontWeight: FontWeight.w700,
                                     color: context.colors.text,
                                     fontSize: 10,
                                   ),
                                 ),
-                              ]),
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'CHANGE',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 12,
+                                Text(
+                                  listing.quote.usd.marketCap
+                                      .toStringAsFixed(2),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 14,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                '${percentChange1h.toStringAsFixed(2)}%',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: color,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const AppSpacing(v: 30),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'PRICE CHART',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 10,
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  'DILUTED MARKET CAP',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 10,
+                                  ),
                                 ),
-                              ),
-                              LineChart(
-                                width: 50,
-                                height: 20,
-                                data: List<LineChartModel>.generate(
-                                  6,
-                                  (_) => LineChartModel(
-                                      amount: Random().nextDouble() * 10),
+                                Text(
+                                  listing.quote.usd.fullyDilutedMarketCap!
+                                      .toStringAsFixed(2),
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: context.colors.text,
+                                    fontSize: 14,
+                                  ),
                                 ),
-
-                                linePaint: Paint()
-                                  ..strokeWidth = 2.5
-                                  ..style = PaintingStyle.stroke
-                                  ..color = color, // Custom paint for the line
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'VOLUME',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 10,
-                                ),
-                              ),
-                              Text(
-                                listing.quote.usd.volume24h.toStringAsFixed(2),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    const AppSpacing(v: 30),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'MARKET CAP',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 10,
-                                ),
-                              ),
-                              Text(
-                                listing.quote.usd.marketCap.toStringAsFixed(2),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'DILUTED MARKET CAP',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 10,
-                                ),
-                              ),
-                              Text(
-                                listing.quote.usd.fullyDilutedMarketCap!
-                                    .toStringAsFixed(2),
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  color: context.colors.text,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    const AppSpacing(v: 50),
-                  ],
+                        ],
+                      ),
+                      const AppSpacing(v: 50),
+                    ],
+                  ),
                 ),
               ),
             ],
@@ -241,5 +252,19 @@ class MarketBottomSheet extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  double getSpaceValue(BuildContext context) {
+    final bool isSmallScreen = ResponsiveWidget.isSmallScreen(context);
+    final bool isMediumScreen = ResponsiveWidget.isMediumScreen(context);
+    final bool isCustomScreen = ResponsiveWidget.isCustomSize(context);
+
+    return isSmallScreen
+        ? 12
+        : isCustomScreen
+            ? 20
+            : isMediumScreen
+                ? 28
+                : 40;
   }
 }
