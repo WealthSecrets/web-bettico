7297d363d8933244648c62dde32c254d2aef0e24
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-13 11:53:40 +0000

    implement the search usecase in the explore controller

--- lib/features/betticos/presentation/explore/getx/explore_bindings.dart
+++ lib/features/betticos/presentation/explore/getx/explore_bindings.dart
@@ -1,5 +1,6 @@
 import 'package:betticos/features/betticos/domain/usecases/post/explore_posts.dart';
 import 'package:betticos/features/betticos/domain/usecases/post/fetch_hashtags.dart';
+import 'package:betticos/features/betticos/domain/usecases/post/search_posts.dart';
 import 'package:betticos/features/betticos/presentation/explore/getx/explore_controller.dart';
 import 'package:get/get.dart';
 
@@ -7,9 +8,9 @@ class ExploreBindings {
   static void dependencies() {
     Get.put<ExploreController>(
       ExploreController(
-        explorePosts: ExplorePosts(betticosRepository: Get.find()),
-        fetchHashtags: FetchHashtags(betticosRepository: Get.find()),
-      ),
+          explorePosts: ExplorePosts(betticosRepository: Get.find()),
+          fetchHashtags: FetchHashtags(betticosRepository: Get.find()),
+          searchPosts: SearchPosts(betticosRepository: Get.find())),
       permanent: true,
     );
   }
--- lib/features/betticos/presentation/explore/getx/explore_controller.dart
+++ lib/features/betticos/presentation/explore/getx/explore_controller.dart
@@ -3,6 +3,7 @@ import 'package:betticos/features/betticos/data/models/post/hashtag_model.dart';
 import 'package:betticos/features/betticos/data/models/post/post_model.dart';
 import 'package:betticos/features/betticos/domain/usecases/post/explore_posts.dart';
 import 'package:betticos/features/betticos/domain/usecases/post/fetch_hashtags.dart';
+import 'package:betticos/features/betticos/domain/usecases/post/search_posts.dart';
 import 'package:dartz/dartz.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -16,10 +17,12 @@ class ExploreController extends GetxController {
   ExploreController({
     required this.explorePosts,
     required this.fetchHashtags,
+    required this.searchPosts,
   });
 
   final ExplorePosts explorePosts;
   final FetchHashtags fetchHashtags;
+  final SearchPosts searchPosts;
 
   RxList<Post> posts = <Post>[].obs;
   RxList<Hashtag> hashtags = <Hashtag>[].obs;
@@ -52,6 +55,33 @@ class ExploreController extends GetxController {
         page: pageK.value,
         size: 100,
         leagueId: 1,
+      ),
+    );
+    failureOrResult.fold<void>(
+      (Failure failure) {
+        isLoading(false);
+        pagingController.error = failure;
+      },
+      (PaginatedResponseData<Post> response) {
+        isLoading(false);
+        if (response.isLastPage) {
+          pagingController.appendLastPage(response.data);
+        } else {
+          pagingController.appendPage(response.data, response.nextPage);
+        }
+      },
+    );
+  }
+
+  void getFilteredPosts(int pageKey) async {
+    pageK(pageKey);
+    isLoading(true);
+    final Either<Failure, PaginatedResponseData<Post>> failureOrResult =
+        await searchPosts(
+      SearchPageParams(
+        keyword: selectedHashtag.value,
+        page: pageK.value,
+        size: 100,
       ),
     );
     failureOrResult.fold<void>(
