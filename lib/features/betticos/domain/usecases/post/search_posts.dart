db5d59207cbfd850aa54ffa6cd40fff09d57a4af
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-13 13:33:15 +0000

    modify search usecase to use searchresponse model

--- lib/features/betticos/domain/usecases/post/search_posts.dart
+++ lib/features/betticos/domain/usecases/post/search_posts.dart
@@ -1,18 +1,15 @@
-import 'package:betticos/core/models/paginated_response_data.dart';
+import 'package:betticos/features/betticos/domain/response/search_response.dart';
 import 'package:dartz/dartz.dart';
 
 import '/core/core.dart';
-import '/features/betticos/data/models/post/post_model.dart';
 import '/features/betticos/domain/repositories/betticos_repository.dart';
 
-class SearchPosts
-    implements UseCase<PaginatedResponseData<Post>, SearchPageParams> {
+class SearchPosts implements UseCase<SearchResponse, SearchPageParams> {
   SearchPosts({required this.betticosRepository});
   final BetticosRepository betticosRepository;
 
   @override
-  Future<Either<Failure, PaginatedResponseData<Post>>> call(
-      SearchPageParams params) {
+  Future<Either<Failure, SearchResponse>> call(SearchPageParams params) {
     return betticosRepository.searchPosts(
         params.keyword, params.page, params.size);
   }
