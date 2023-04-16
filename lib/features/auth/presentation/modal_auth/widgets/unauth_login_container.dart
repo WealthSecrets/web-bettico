97b9385b9b784cae08abceb14086b9fd93344316
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-16 10:12:07 +0000

    show is loading for login modal

--- lib/features/auth/presentation/modal_auth/widgets/unauth_login_container.dart
+++ lib/features/auth/presentation/modal_auth/widgets/unauth_login_container.dart
@@ -18,201 +18,203 @@ class UnAuthLoginController extends GetWidget<LoginController> {
   Widget build(BuildContext context) {
     return Obx(
       () => Material(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 100),
-          child: AppAnimatedColumn(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Image.asset(AssetImages.logo, height: 30, width: 30),
-              const SizedBox(height: 16),
-              const Text(
-                'Login to join activities on Xviral.',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+        child: AppLoadingBox(
+          loading: controller.isLoading.value,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100),
+            child: AppAnimatedColumn(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Image.asset(AssetImages.logo, height: 30, width: 30),
+                const SizedBox(height: 16),
+                const Text(
+                  'Login to join activities on Xviral.',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 32),
-              Stack(
-                children: <Widget>[
-                  if (controller.isPhone.value)
-                    AppTextInput(
-                      labelText: 'email_address'.tr.toUpperCase(),
-                      initialValue: '',
-                      backgroundColor: context.colors.primary.shade100,
-                      prefixIcon: Icon(
-                        Ionicons.mail_outline,
-                        color: context.colors.hintLight,
-                        size: 18,
+                const SizedBox(height: 32),
+                Stack(
+                  children: <Widget>[
+                    if (controller.isPhone.value)
+                      AppTextInput(
+                        labelText: 'email_address'.tr.toUpperCase(),
+                        initialValue: '',
+                        backgroundColor: context.colors.primary.shade100,
+                        prefixIcon: Icon(
+                          Ionicons.mail_outline,
+                          color: context.colors.hintLight,
+                          size: 18,
+                        ),
+                        lableStyle: TextStyle(
+                          color: context.colors.primary,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 10,
+                        ),
+                        errorStyle: TextStyle(
+                          color: context.colors.error,
+                          fontSize: 12,
+                        ),
+                        validator: controller.validateEmail,
+                        onChanged: controller.onEmailInputChanged,
+                      )
+                    else
+                      AppPhoneInput(
+                        labelText: 'phone_number'.tr.toUpperCase(),
+                        prefixIcon: Icon(
+                          Ionicons.call_outline,
+                          color: context.colors.hintLight,
+                          size: 18,
+                        ),
+                        initialValue: '',
+                        textInputType: TextInputType.phone,
+                        backgroundColor: context.colors.primary.shade100,
+                        lableStyle: TextStyle(
+                          color: context.colors.primary,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 10,
+                        ),
+                        validator: controller.validatePhone,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly,
+                          FilteringTextInputFormatter.deny(' '),
+                        ],
+                        onChanged: controller.onPhoneInputChanged,
                       ),
-                      lableStyle: TextStyle(
-                        color: context.colors.primary,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 10,
-                      ),
-                      errorStyle: TextStyle(
-                        color: context.colors.error,
-                        fontSize: 12,
-                      ),
-                      validator: controller.validateEmail,
-                      onChanged: controller.onEmailInputChanged,
-                    )
-                  else
-                    AppPhoneInput(
-                      labelText: 'phone_number'.tr.toUpperCase(),
-                      prefixIcon: Icon(
-                        Ionicons.call_outline,
-                        color: context.colors.hintLight,
-                        size: 18,
-                      ),
-                      initialValue: '',
-                      textInputType: TextInputType.phone,
-                      backgroundColor: context.colors.primary.shade100,
-                      lableStyle: TextStyle(
-                        color: context.colors.primary,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 10,
-                      ),
-                      validator: controller.validatePhone,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly,
-                        FilteringTextInputFormatter.deny(' '),
-                      ],
-                      onChanged: controller.onPhoneInputChanged,
-                    ),
-                  const SizedBox(height: 8),
-                  Positioned(
-                    right: 10,
-                    child: AnimatedSwitcher(
-                      reverseDuration: Duration.zero,
-                      transitionBuilder:
-                          (Widget? child, Animation<double> animation) {
-                        final Animation<double> offset =
-                            Tween<double>(begin: 0, end: 1.0)
-                                .animate(animation);
-                        return ScaleTransition(scale: offset, child: child);
-                      },
-                      switchInCurve: Curves.elasticOut,
-                      duration: const Duration(milliseconds: 700),
-                      child: RichText(
-                        text: TextSpan(
-                          text: controller.isPhone.value
-                              ? 'login_mobile'.tr
-                              : 'login_email'.tr,
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              controller.togglePhoneVisibility(
-                                  !controller.isPhone.value);
-                            },
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w800,
-                            fontSize: 12,
+                    const SizedBox(height: 8),
+                    Positioned(
+                      right: 10,
+                      child: AnimatedSwitcher(
+                        reverseDuration: Duration.zero,
+                        transitionBuilder:
+                            (Widget? child, Animation<double> animation) {
+                          final Animation<double> offset =
+                              Tween<double>(begin: 0, end: 1.0)
+                                  .animate(animation);
+                          return ScaleTransition(scale: offset, child: child);
+                        },
+                        switchInCurve: Curves.elasticOut,
+                        duration: const Duration(milliseconds: 700),
+                        child: RichText(
+                          text: TextSpan(
+                            text: controller.isPhone.value
+                                ? 'login_mobile'.tr
+                                : 'login_email'.tr,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                controller.togglePhoneVisibility(
+                                    !controller.isPhone.value);
+                              },
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 12,
+                            ),
                           ),
                         ),
                       ),
                     ),
+                  ],
+                ),
+                AppTextInput(
+                  obscureText: true,
+                  labelText: 'password'.tr.toUpperCase(),
+                  showObscureTextToggle: true,
+                  backgroundColor: context.colors.primary.shade100,
+                  prefixIcon: Icon(
+                    Ionicons.lock_closed_outline,
+                    color: context.colors.hintLight,
+                    size: 18,
                   ),
-                ],
-              ),
-              AppTextInput(
-                obscureText: true,
-                labelText: 'password'.tr.toUpperCase(),
-                showObscureTextToggle: true,
-                backgroundColor: context.colors.primary.shade100,
-                prefixIcon: Icon(
-                  Ionicons.lock_closed_outline,
-                  color: context.colors.hintLight,
-                  size: 18,
-                ),
-                lableStyle: TextStyle(
-                  color: context.colors.primary,
-                  fontWeight: FontWeight.w700,
-                  fontSize: 10,
-                ),
-                errorStyle: TextStyle(
-                  color: context.colors.error,
-                  fontSize: 12,
-                ),
-                validator: controller.validatePassword,
-                onChanged: controller.onPasswordInputChanged,
-              ),
-              const SizedBox(height: 10),
-              Align(
-                alignment: Alignment.centerRight,
-                child: RichText(
-                  text: TextSpan(
-                    text: 'forgot_pass'.tr,
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Get.toNamed<void>(AppRoutes.forgot);
-                      },
-                    style: TextStyle(
-                      color: context.colors.error,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 12.0,
-                    ),
+                  lableStyle: TextStyle(
+                    color: context.colors.primary,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 10,
                   ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              AppButton(
-                enabled: controller.formIsValid,
-                padding: EdgeInsets.zero,
-                borderRadius: AppBorderRadius.largeAll,
-                backgroundColor: context.colors.primary,
-                onPressed: () {
-                  controller.login(context);
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  'sign_in'.tr.toUpperCase(),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                  errorStyle: TextStyle(
+                    color: context.colors.error,
                     fontSize: 12,
                   ),
+                  validator: controller.validatePassword,
+                  onChanged: controller.onPasswordInputChanged,
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextButton(
-                onPressed: () {
-                  Get.toNamed<void>(AppRoutes.signup);
-                },
-                child: Center(
+                const SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
                   child: RichText(
-                    textAlign: TextAlign.center,
                     text: TextSpan(
-                      style: context.body2,
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'no_account'.tr,
-                          style: TextStyle(
-                            color: context.colors.text,
-                            fontSize: 12,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'register_now'.tr,
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Get.toNamed<void>(AppRoutes.signup);
-                            },
-                          style: TextStyle(
-                            color: context.colors.primary,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
+                      text: 'forgot_pass'.tr,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Get.toNamed<void>(AppRoutes.forgot);
+                        },
+                      style: TextStyle(
+                        color: context.colors.error,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 12.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-            ],
+                const SizedBox(height: 16),
+                AppButton(
+                  enabled: controller.formIsValid,
+                  padding: EdgeInsets.zero,
+                  borderRadius: AppBorderRadius.largeAll,
+                  backgroundColor: context.colors.primary,
+                  onPressed: () {
+                    controller.login(context);
+                  },
+                  child: Text(
+                    'sign_in'.tr.toUpperCase(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextButton(
+                  onPressed: () {
+                    Get.toNamed<void>(AppRoutes.signup);
+                  },
+                  child: Center(
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        style: context.body2,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'no_account'.tr,
+                            style: TextStyle(
+                              color: context.colors.text,
+                              fontSize: 12,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'register_now'.tr,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Get.toNamed<void>(AppRoutes.signup);
+                              },
+                            style: TextStyle(
+                              color: context.colors.primary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       ),
