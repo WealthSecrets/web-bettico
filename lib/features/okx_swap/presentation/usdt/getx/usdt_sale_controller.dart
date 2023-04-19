e8e6d997a01785dbf23f8280c92dd7436df9e368
Richmond Blankson <mensahrichmondb@gmail.com>
2023-04-19 12:01:54 +0000

    add controller and validations and onchanged for fiatAmount and address

--- lib/features/okx_swap/presentation/usdt/getx/usdt_sale_controller.dart
+++ lib/features/okx_swap/presentation/usdt/getx/usdt_sale_controller.dart
@@ -2,4 +2,41 @@ import 'package:get/get.dart';
 
 class UsdtSaleController extends GetxController {
   UsdtSaleController();
+
+  RxString fiatAmount = ''.obs;
+  RxString walletAddress = ''.obs;
+
+  void onAmountInputChanged(String value) {
+    fiatAmount(value);
+  }
+
+  void onWalletAddressChanged(String value) {
+    walletAddress(value);
+  }
+
+  String? validateAmount(String stringAmount) {
+    final double? amount = double.tryParse(stringAmount);
+    if (amount == null) {
+      return 'Invalid amount';
+    }
+    if (amount < 1) {
+      return r'Minimum of $1';
+    }
+    if (amount > 1000) {
+      return r'Maximum of $10,000';
+    }
+    return null;
+  }
+
+  String? validateAddress(String? address) {
+    String? errorMessage;
+    if (address!.isEmpty) {
+      errorMessage = 'Please enter wallet address';
+    }
+    return errorMessage;
+  }
+
+  bool get formIsValid =>
+      validateAmount(fiatAmount.value) == null ||
+      validateAddress(walletAddress.value) == null;
 }
